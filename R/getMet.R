0c72cac11a3d907b353604efd9dbf0eefe2411d5
Grad <andy.sommerlot@gmail.com>
2016-03-09 17:01:08 -0500

    added wunderground 10 day forecast api as data source

--- R/getMet.R
+++ R/getMet.R
@@ -8,8 +8,8 @@
 #						hydrologic modeling													  #
 # 				central function of the hydroMet packages                                     #
 # Currently supports 																		  #
-#		sources: CFSR                                                                         #
-#		formats: SWAT                                                                         #              
+#		sources: CFSR, wunderground 10 day forecast api                                                                         #
+#		formats: SWAT                                                                         #
 ###############################################################################################
 
 #' Gets met data from a specified source and creates model input files in the specified format
@@ -17,23 +17,34 @@
 #' @param dataSource - Source of met data from predefined source list. Currenly only 'cfsr' is supported.
 #' @param outFormat - Format of met data output from predefined source. Currently only 'swat' is supported.
 #' @param outDir - Directory where ouput files will be saved
+#' @param apiKey - String input of api key for selected data source if required.
 #' @return returns specified  met data in specified format
-#' @examples 
+#' @examples
 #' \dontrun{
 #' locations = data.frame(lat = 38, lon = 79)
 #' outDir = "test"
-#' getMet(locations=locations, outDir=outDir)
+#' getMet(locations=locations, outDir=outDir, dataSource = 'cfsr', outFormat = 'swat')
 #' }
-#' @export 
+#' @export
 
 
-getMet <-  function(locations, dataSource = 'cfsr', outFormat = 'swat', outDir = getwd()){
-  
-  if(dataSource != 'cfsr'){
-    stop('The getMet function does not yet have support for data sources other than the cfsr model\n', 'Set dataSource to cfsr to continue')
-  } else if(outFormat != 'swat') {
-    stop('The getMet function does not yet have support for output formats other than the swat model\n', 'Set outFormat to swat to continue')
-  }else{
+getMet <-  function(locations, dataSource = 'cfsr', outFormat = 'swat', outDir = getwd(), apiKey = '') {
+
+  if(dataSource != 'cfsr' && dataSource != 'wunderForecast'){
+      stop('The getMet function does not yet have support for this data source\n', 'Set dataSource to "cfsr" or "wunderForecast" to continue')
+  }
+  if(outFormat != 'swat') {
+    stop('The getMet function does not yet have support for output formats other than the swat model\n', 'Set outFormat to "swat" to continue')
+  }
+  if(dataSource == 'cfsr') {
     getSWATcfsr(locations, outDir = outDir)
   }
+  if(dataSource == 'wunderForecast' && apiKey == '') {
+      stop('Using wunderForecast requires and api key. Get one for free at https://www.wunderground.com/weather/api/d/pricing.html. Select the free version of the ANVIL plan.')
+  }
+  if(dataSource == 'wunderForecast' && apiKey != '') {
+    getSWATwunderForecast(locations, outDir = outDir, apiKey = apiKey)
+  }
 }
+
+
